2f376144e517ba34fabda288905409d270310b58
scottandrewp <70963325+scottandrewp@users.noreply.github.com>
2021-02-03 18:21:33 -0700

    Add project files.

---
+++ Models/MovieList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public class MovieList
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        public static IEnumerable<Movie> Movies => movies;
+
+        public static void AddMovie(Movie movie)
+        {
+            movies.Add(movie);
+        }
+    }
+}
